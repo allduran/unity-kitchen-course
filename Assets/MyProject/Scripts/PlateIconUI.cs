60e6c6b46cae3df9be0fb78996e50d4a8e21509a
duran <cheshire.dd96@gmail.com>
2026-05-05 08:47:25 +0200

    Shader Graphs Study

---
+++ Assets/MyProject/Scripts/PlateIconUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlateIconUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredienteAdded += PlateKitchenObject_OnIngredienteAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredienteAdded(object sender, PlateKitchenObject.OnIngredienteAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconsSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+        }
+    }
+}
--- Assets/MyProject/Scripts/PlateKitchenObject.cs
+++ Assets/MyProject/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredienteAddedEventArgs> OnIngredienteAdded;
+    public class OnIngredienteAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
     private List<KitchenObjectSO> kitchenObjectSOList;
@@ -28,7 +35,17 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredienteAdded?.Invoke(this, new OnIngredienteAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
